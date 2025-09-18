539bc26542a5b7689d1510450552fa2eb5e94af6
tonyo <arabit.tr@gmail.com>
2025-09-18 23:09:27 +0800

    updated button children behavior: no wrap on texts

--- portfolio/src/components/Button.jsx
+++ portfolio/src/components/Button.jsx
@@ -2,7 +2,7 @@ export function Button({children, type = '', onClick = () => {}}, className = ""
     return (
         <div className={`button-wrapper`}>
             <div className={`button-component text-bg-${type} bg-${type}`} onClick={onClick}>
-                <div className={className}>{children}</div>
+                <div className={className + "text-nowrap"}>{children}</div>
             </div>
         </div>
     )
